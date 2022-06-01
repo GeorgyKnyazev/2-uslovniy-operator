1d699f75ab9411fb30f61854f158556e0ea4f441
Георгий Князев <saint_georgy@yahoo.com>
2022-06-01 12:20:16 +0300

    Добавьте файлы проекта.

---
+++ 2 uslovniy operator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2_uslovniy_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyWord = "exit";
+            string userInput ="";
+
+
+            while (keyWord != userInput)
+            {
+                Console.WriteLine("Введите слово :");
+                userInput = Console.ReadLine();
+            }
+            
+        }
+    }
+}
